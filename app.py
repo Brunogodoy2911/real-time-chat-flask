830ed6d435ddb593e98bafce27672d8cf3642c3a
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-11-23 21:12:10 -0300

    adiciona funcionalidade de upload de foto de perfil e atualiza requisitos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request, render_template, current_app
+import os
+from flask import Flask, jsonify, request, render_template, current_app, redirect, url_for
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 import bcrypt
 from repository.database import db
@@ -6,6 +7,14 @@ from datetime import datetime
 from db_models.message import Message
 from db_models.user import User
 from flask_socketio import SocketIO, emit
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "static/profile_pics"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+def allowed_file(filename):
+    return "." in filename and \
+        filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -56,6 +65,29 @@ def create_user():
     
     return jsonify({ "error": "Invalid credentials" }), 400
 
+@app.route("/api/upload_photo", methods=["POST"])
+@login_required
+def upload_photo():
+    
+        if "foto_perfil" not in request.files:
+            return redirect(url_for("index"))
+        
+        file = request.files["foto_perfil"]
+        
+        if file.filename == "":
+            return redirect(url_for("index"))
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            
+            current_user.profile_image = filename
+            db.session.commit()
+            
+            return redirect(url_for("index"))
+        
+        return redirect(url_for("index"))
+        
 @app.route("/api/send_message", methods=["POST"])
 @login_required
 def send_message():
